df1a0c40217be975dbc5d02375cfb31009662402
Akshay Sharma <fxn769@gmail.com>
2022-10-15 13:04:26 +0530

    splash Screen added

--- app/src/main/java/io/ak1/nytimes/MainActivity.kt
+++ app/src/main/java/io/ak1/nytimes/MainActivity.kt
@@ -3,15 +3,17 @@ package io.ak1.nytimes
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import io.ak1.nytimes.ui.screens.components.RootComponent
 import io.ak1.nytimes.ui.screens.home.StoriesViewModel
 import org.koin.android.ext.android.inject
 
 
-class MainComposeActivity : ComponentActivity() {
+class MainActivity : ComponentActivity() {
 
     private val liveViewModel by inject<StoriesViewModel>()
     override fun onCreate(savedInstanceState: Bundle?) {
+        installSplashScreen()
         super.onCreate(savedInstanceState)
         setContent {
             RootComponent(liveViewModel, window)
